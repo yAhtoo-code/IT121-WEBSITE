14da9c39f4a5176b92d0eefa31d7f0afae6e57f0
yAhtoo-code <delacruztimothy55@gmail.com>
2024-12-11 13:56:10 +0800

    timo

--- Main_page.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('.dropbtn').addEventListener('click', function () {
-    const dropdown = this.nextElementSibling;
-    dropdown.style.display = dropdown.style.display === 'block' ? 'none' : 'block';
-  });
